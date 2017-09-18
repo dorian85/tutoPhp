3c6a1195a8b355b412d626cd180250ebc8599d30
Dorian Bourgeois <dorianbourgeois85@gmail.com>
2017-09-18 11:14:02 +0200

    Structure du blog

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<title>Tuto Blog</title>
+		<link rel="stylesheet" type="text/css" href="reset.css">
+		<link rel="stylesheet" type="text/css" href="main.css">
+	</head>
+
+	<body>
+
+		<div class="wrapper">
+			<div class="articles-list">
+				<article>
+					<h1>Mon premier article</h1>
+					<div class="meta-data">
+						<span class="category">Catégorie : catégorie 1</span> -
+						<span class="creation-date">Date : 18/09/2017</span> -
+						<span class="author">Auteur : Dorian</span>
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor ad sit delectus aliquam! Soluta distinctio, nisi ab. Recusandae explicabo, nam et maiores possimus architecto, aliquid, commodi soluta eveniet incidunt provident.</p>
+				</article>
+			</div>
+		</div>
+		
+	</body>
+</html>
